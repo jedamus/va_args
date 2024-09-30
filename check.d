ea0c663fdf00d71c796c5712fd11de568bf68599
Leander Jedamus <ljedamus@web.de>
2024-10-01 00:41:07 +0200

    added the_version.texinfo

--- check.d
+++ check.d
@@ -1,1 +1,1 @@
-check.info check.xml check.txt check.d: Makefile Makefile.documentation check.texinfo version.texinfo
+check.info check.xml check.txt check.d: Makefile Makefile.documentation check.texinfo the_version.texinfo
